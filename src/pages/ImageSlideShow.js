34c6043eacb53d6bf8cda9d70711237fb65e7a93
Justus Massmann <justusmassmann@online.de>
2022-04-24 17:28:21 +0200

    Add imageslideshow to navigation and react router

---
+++ src/pages/ImageSlideShow.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default function ImageSlideShow() {
+  return (
+    <main>
+      <SlideButton />
+    </main>
+  );
+}
+
+const SlideButton = styled.button``;
