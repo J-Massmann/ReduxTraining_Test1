1ea4ee31edb13ed445f3dcc3185b49186cf42218
Justus Massmann <justusmassmann@online.de>
2022-04-24 22:47:04 +0200

    Initial commit

---
+++ src/data/AboutTexts.js
@@ -0,0 +1,16 @@
+const Text = [
+  {
+    name: 'About us',
+    text: 'Adipisicing fugiat commodo magna irure eiusmod sit reprehenderit enim sint sint excepteur aliqua cillum. Sint exercitation exercitation Lorem eiusmod. Enim proident proident qui eu non enim reprehenderit sint. Id fugiat non culpa sint proident occaecat sint dolore ad nostrud veniam. Laboris occaecat sit do excepteur deserunt. Laboris aute est nisi fugiat sint nostrud cillum fugiat pariatur sunt. Deserunt deserunt dolore nostrud ex exercitation laboris occaecat laborum eu cillum consectetur tempor nostrud eu.',
+  },
+  {
+    name: 'Vision',
+    text: 'Adipisicing fugiat commodo magna irure eiusmod sit reprehenderit enim sint sint excepteur aliqua cillum. Sint exercitation exercitation Lorem eiusmod. Enim proident proident qui eu non enim reprehenderit sint. Id fugiat non culpa sint proident occaecat sint dolore ad nostrud veniam. Laboris occaecat sit do excepteur deserunt. Laboris aute est nisi fugiat sint nostrud cillum fugiat pariatur sunt. Deserunt deserunt dolore nostrud ex exercitation laboris occaecat laborum eu cillum consectetur tempor nostrud eu.',
+  },
+  {
+    name: 'History',
+    text: 'Adipisicing fugiat commodo magna irure eiusmod sit reprehenderit enim sint sint excepteur aliqua cillum. Sint exercitation exercitation Lorem eiusmod. Enim proident proident qui eu non enim reprehenderit sint. Id fugiat non culpa sint proident occaecat sint dolore ad nostrud veniam. Laboris occaecat sit do excepteur deserunt. Laboris aute est nisi fugiat sint nostrud cillum fugiat pariatur sunt. Deserunt deserunt dolore nostrud ex exercitation laboris occaecat laborum eu cillum consectetur tempor nostrud eu.',
+  },
+];
+
+export default Text;
